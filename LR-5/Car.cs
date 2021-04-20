cb8d469d76c0799ec3ea156738bc74eabeeb398e
Artem Bokun <79195348+relsa228@users.noreply.github.com>
2021-04-21 00:01:50 +0300

    New lab

---
+++ LR-5/Car.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace LR_5
+{
+    class Car : Vehicle
+    {
+        private int _parking;
+        public Car()
+        {
+            _parking = 0;
+        }
+
+        public int this[string status]
+        {
+            set
+            {
+                switch (status)
+                {
+                    case "PStatus":
+                        _parking = value;
+                        break;
+                    case "SStatus":
+                        Speed = value;
+                        break;
+                    case "CStatus":
+                        Coordinates = value;
+                        break;
+                }
+            }
+            get
+            {
+                switch (status)
+                {
+                    case "PStatus":
+                        return _parking;
+                    case "SStatus":
+                        return Speed;
+                    case "CStatus":
+                        return Coordinates;
+                    default:
+                        return 0;
+                }
+            }
+        }
+
+        public void Revers()
+        {
+            Speed = Speed * (-1);
+            _parking = 0;
+        }
+
+        public void Drive()
+        {
+            _parking = 0;
+        }
+
+        public void Park()
+        {
+            _parking = 1;
+            Speed = 0;
+        }
+
+        public override void Move(int time)
+        {
+            if (_parking == 1)
+            {
+                Console.WriteLine("Переключите коробку в режим вождения");
+            }
+            else
+            {
+                Coordinates = Coordinates + Speed * time;
+            }
+        }
+        
+        public override void Overclocking(int time)
+        {
+            if (_parking == 1)
+            {
+                Console.WriteLine("Переключите коробку в режим вождения");
+            }
+            else
+            {
+                Speed = Acceleration * time + Speed;
+            }
+        }
+    }
+}
---
+++ LR-5/Vehicle.cs
@@ -0,0 +1,41 @@
+﻿namespace LR_5
+{
+    class Vehicle
+    {
+        protected int Speed;
+        protected int Coordinates;
+        protected int Acceleration;
+
+        protected Vehicle()
+        {
+            Speed = 0;
+            Coordinates = 0;
+            Acceleration = 0;
+        }
+        
+        public void Breaking(int speed, int time)
+        {
+            Speed = Speed - speed * time;
+            if (Speed < 0)
+                Speed = 0;
+        }
+
+        public void SetAcceleration(int acc)
+        {
+            if (acc > 0)
+                Acceleration = acc;
+            else
+                Acceleration = 0; 
+        }
+        
+        public virtual void Overclocking(int time)
+        {
+            Speed = Acceleration * time + Speed;
+        }
+        
+        public virtual void Move(int time)
+        {
+            Coordinates = Coordinates + Speed * time;
+        }
+    }
+}
