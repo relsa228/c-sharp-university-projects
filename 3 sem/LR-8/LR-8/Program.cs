f610364592da68db5dc2305607de49408bea0047
Artem Bokun <79195348+relsa228@users.noreply.github.com>
2021-10-14 19:08:21 +0300

    LR-8

---
+++ 3 sem/LR-8/LR-8/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LR_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
